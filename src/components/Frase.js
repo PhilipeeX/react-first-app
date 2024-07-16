000ed97c798f77a5a83b61d6b7b80aed79e61be0
PhilipeeX <dev.philipe@outlook.com>
2024-07-15 22:32:39 -0300

    feat: Creates some components to learn about them

---
+++ src/components/Frase.js
@@ -0,0 +1,9 @@
+function frase() {
+  return(
+    <div>
+      <p>Este é um componente que contem uma frase!</p>
+    </div>
+  )
+}
+
+export default frase;
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,15 @@
+import Frase from './Frase';
+
+function HelloWorld() {
+  return(
+    <div>
+      <Frase />
+        <h1>Testando meu primeiro componente</h1>
+        <p>O componente frase foi importado, foi exibido acima e será exibido abaixo</p>
+      <Frase />
+    </div>
+  )
+}
+
+
+export default HelloWorld;
